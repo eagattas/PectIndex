dec2cbb2a7c45a1973ada5b3716ec2f861a3c501
Evan Brunt <ebrunt@umich.edu>
2017-10-28 14:31:14 -0400

    Adding area and surface files

---
+++ area.h
@@ -0,0 +1,12 @@
+#ifndef AREA_H
+#define AREA_H
+
+#include <cmath>
+
+double distance(double x1, double x2, double y1, double y2);
+double areaTrapezoid(double l1, double l2, double l3, double l4);
+double areaTriangle(double l1, double l2, double l3);
+double defectArea(Vertex v1, Vertex v2, QVector<QPair<Vertex, Vertex>> defectSegments);
+double chestArea();
+
+#endif // AREA_H
---
+++ surface.h
@@ -0,0 +1,21 @@
+#ifndef SURFACE_H
+#define SURFACE_H
+
+#include <QObject>
+#include <QDebug>
+#include <QtDataVisualization/Q3DSurface>
+#include <QtDataVisualization/QSurfaceDataProxy>
+#include <QtDataVisualization/QHeightMapSurfaceDataProxy>
+#include <QtDataVisualization/QSurface3DSeries>
+
+class Surface : public Q_OBJECT
+{
+    public:
+        void Surface::createSurfaceModel(const QVector<Vertex> & vertices, const QVector<Face> & faces);
+        void Surface::resetModel();
+
+    private:
+        Q3DSurface* model = new Q3DSurface();
+}
+
+#endif // SURFACE_H
